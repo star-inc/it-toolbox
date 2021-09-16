a8f0885bf1c2405eab6777db59187356e24e8fb7
SuperSonic <supersonic@livemail.tw>
2021-09-17 02:29:25 +0800

    Update PasswordGenerator

--- src/views/PasswordGenerator.vue
+++ src/views/PasswordGenerator.vue
@@ -4,6 +4,13 @@
       <li>Password: <code>{{ secret }}</code></li>
     </ul>
     <a href="javascript:void(0)" @click="refresh">refresh</a>
+    <div>
+      <span>
+        Special thanks:
+        <a href="https://stackoverflow.com/questions/1497481/javascript-password-generator">Gumbo</a>
+        answered the code snippet.
+      </span>
+    </div>
   </div>
 </template>
 
